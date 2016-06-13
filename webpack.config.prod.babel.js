fbf316189bc4d9d0e50bd3ef84efece369ebe3c8
saimeunt <saimeunt@gmail.com>
2016-06-13 10:50:54 +0200

    latest

--- webpack.config.prod.babel.js
+++ webpack.config.prod.babel.js
@@ -2,7 +2,7 @@ import path from 'path';
 import webpack from 'webpack';
 
 export default {
-  devtool: 'source-map',
+  // devtool: 'source-map',
   entry: ['./src/index'],
   output: {
     path: path.join(__dirname, 'dist'),
